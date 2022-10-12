ad2b25e1da08a44c0ada158c26fce0e099d3d98b
Daniel Yu <yudan@oregonstate.edu>
2022-10-11 17:11:40 -0700

    Adding navigation and login screen
    Adding React Navigation and creating a LoginScreen

---
+++ screens/LoginScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, Button } from 'react-native'
+
+
+const LoginScreen = () => {
+
+    return (
+        <View>
+            <Text>Hello Topguns!</Text>
+        </View>
+    );
+};
+
+export default LoginScreen
+
+const styles = StyleSheet.create({});
