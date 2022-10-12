f9102fbee5ec23f57c3a6493319297a89eb737d2
Daniel Yu <yudan@oregonstate.edu>
2022-10-11 17:28:21 -0700

    Add Formik for user input / forms
    Login screen has email/password fields, login/register buttons in center.

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,16 +1,49 @@
 import React from 'react'
 import { StyleSheet, Text, TextInput, TouchableOpacity, View, Button } from 'react-native'
+import { Formik } from 'formik';
 
 
 const LoginScreen = () => {
-
     return (
-        <View>
-            <Text>Hello Topguns!</Text>
+        <View style={styles.formikContainer}>
+            <Formik >
+                {() => (
+                    <View>
+                        <View>
+                            <TextInput
+                                placeholder='E-mail...'                                
+                            />
+                            <TextInput
+                                placeholder='Password...'
+                            />
+                        </View>
+                        <TouchableOpacity
+                            style={styles.button}
+                        >
+                            <Text>Login</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={styles.button}
+                        >
+                            <Text>Register</Text>
+                        </TouchableOpacity>
+                    </View>
+                )}
+            </Formik>
         </View>
     );
 };
 
 export default LoginScreen
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    formikContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+    },
+    button: {
+        backgroundColor: 'orange',
+        borderRadius: 10,
+    }
+});
