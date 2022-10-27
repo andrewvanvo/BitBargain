4132b475e50ad471d22aaa66f28d8e87fa530115
Andrew Vo <andrew.van.vo@gmail.com>
2022-10-27 16:05:18 -0400

    DB props accessible in List component

--- screens/SavedListsScreen.js
+++ screens/SavedListsScreen.js
@@ -6,12 +6,12 @@ import { useNavigation } from '@react-navigation/native';
 import { collection, query, where, getDocs, setDoc, doc, onSnapshot } from "firebase/firestore";
 import { firestore } from '../firebase';
 
-const currShoppingList = new Set();
 class List extends React.Component {
     constructor(props) {
         super(props);
         this.item = props.item;
         this.navigation = props.navigation; //nav prop accesible from parent screen
+        this.dbProducts = props.savedProducts //product collection snapshot passed down
     }
     render() {
         return (
@@ -23,15 +23,15 @@ class List extends React.Component {
     }
 
     navigateToCurrentList = async(item)=>{
-        console.log(this.props)
         var cartItems = [];
+        var productList =[];
         try {
-            var test = this.item.userID
-            //this.item.productArray.forEach(async function (product){
-            //    cartItems.push(product)
-            //});
-            //await AsyncStorage.setItem('@storage_Key', JSON.stringify(cartItems));
-            console.log(test)
+            this.item.productArray.forEach((product) =>{
+                productList.push(product)
+            })
+            console.log(this.props.dbProducts)
+            await AsyncStorage.setItem('@storage_Key', JSON.stringify(cartItems));
+            //console.log(test)
             //console.log(cartItems);
         } catch (error){
             console.log(error);
@@ -39,9 +39,10 @@ class List extends React.Component {
         this.navigation.navigate('CurrentList')
     };
 }
-
 const SavedListsScreen = ({navigation}) => {
     const [savedLists, setSavedLists] = useState([]);
+    const [savedProducts, setSavedProducts] = useState([]);
+    
     //dynamically update list of saved lists
     useEffect(() => {
         const listsRef = collection(firestore, 'SavedLists'); //name of collection
@@ -51,9 +52,24 @@ const SavedListsScreen = ({navigation}) => {
                 savedLists.push(doc.data());
             });
             setSavedLists(savedLists);
+            
         })
         return () => unsubscribe;
     }, []);
+
+    useEffect(() => {
+        const productsRef = collection(firestore, 'Products'); //name of collection
+        const unsubscribe = onSnapshot(productsRef, (productsSnap) => {
+            const savedProducts= []
+            productsSnap.forEach((doc) => {
+                savedProducts.push(doc.data());
+            });
+            setSavedProducts(savedProducts);
+            //console.log(savedProducts)
+        })
+        return () => unsubscribe;
+    }, []);
+
 
     const renderList = ({ item }) => {
         return (
@@ -61,6 +77,7 @@ const SavedListsScreen = ({navigation}) => {
             <List 
                 item={item}
                 navigation = {navigation} //pull navigation prop from parent screen and pass as prop
+                dbProducts ={savedProducts}
             />
         );
     };
@@ -70,6 +87,7 @@ const SavedListsScreen = ({navigation}) => {
             <View style={styles.listContainer}>
                 <FlatList
                     data={savedLists}
+                    extraData = {savedProducts}
                     renderItem={renderList}
                     keyExtractor={item => item.listName} //listname must be unique, ensure it is when saving list
 
