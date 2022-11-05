1cebad2ae729353db2dd0167d747eaadf3005750
Andrew Vo <andrew.van.vo@gmail.com>
2022-11-05 09:33:16 -0400

    geolocation call works, and subsequent gmaps api call works

--- screens/UpdateSelectStoreScreen.js
+++ screens/UpdateSelectStoreScreen.js
@@ -1,66 +1,69 @@
 
 import React, { useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { Platform, Text, View, StyleSheet, FlatList } from 'react-native';
+import { Platform, Text, View, StyleSheet, FlatList, Button } from 'react-native';
 
 import * as Location from 'expo-location';
 
 const UpdateSelectStoreScreen = ({navigation}) => {
   //public gmaps api key and variables
   let api_key = 'AIzaSyApOw3rA0vexD0o73xlDZN8OoxIFgefUdY'
-  let radius = '25000' //radius in m
+  let radius = '15000' //radius in m
   let type = 'electronic_store' //https://developers.google.com/maps/documentation/places/web-service/supported_types
 
-  //expo location module
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
 
-  //google maps places api
-  const [isLoading, setLoading] = useState(true);
+  const [location, setLocation] = useState(null);
   const [data, setData] = useState([]);
 
-  //get expo location details
-  useEffect(() => {
-    (async () => {
-      
+  useEffect(()=>{
+    let runHook = true
+    const fetchLocation = async ()=>{
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
-
+          setErrorMsg('Permission to access location was denied');
+          return;
+        }
       let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-      setLoading(false)
-    })();
-  }, []);
+      if(runHook){
+        setLocation(location);
+      }
+    }
+    fetchLocation()
+    return () => runHook = false
+  }, [])
 
-  let lat;
-  let long;
-  if (errorMsg) {
-    console.log(error)
-  } else if (location) {
+  let lat = '44.5666670'
+  let long = '-123.2833330'
+  if (location){
     lat = location['coords']['latitude']
     long = location['coords']['longitude']
   }
+  
+  //gmaps api
+  useEffect(() => {
+    async function fetchGoogle(){
+      await fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&radius=${radius}&type=${type}&keyword=computer&key=${api_key}`)
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+    }
+    fetchGoogle();
+    //console.log(data)
+    data['results'].forEach(element => {
+      console.log(element.name)
+    });
 
-  //gmaps places api call
-  //useEffect(() => {
-  //  fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}${long}&radius=${radius}&type=${type}&keyword=computer&key=${api_key}`)
-  //    .then((response) => response.json())
-  //    .then((json) => setData(json))
-  //    .catch((error) => console.error(error))
-  //    .finally(() => setLoading(false));
-  //}, []);
-
+    }, [location]);
+  
   return (
 
     <View style={styles.container}>
-      {isLoading ? <Text>Getting Location</Text> : 
-      ( <View >
-          <Text> {lat} {long}</Text>
-        </View>
-      )}
+      
+      <View >
+        <Text> {lat} </Text>
+        <Text></Text>
+      </View>
+      
     </View>
   );
 }
