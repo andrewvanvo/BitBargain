6df9f4a6c52d3fae772df26a2552cf63f2a6f3d0
Daniel Yu <yudan@oregonstate.edu>
2022-11-12 09:05:28 -0800

    Sample functions for using subcollection.

--- screens/TestingScreen.js
+++ screens/TestingScreen.js
@@ -14,6 +14,24 @@ const TestingScreen = ({navigation}) => {
     const navigateToScan = ()=>{
         navigation.navigate('Scanning');
     };
+
+
+    const updateProductPrice = async () => {
+        const targetProductId = '831EcSKFAbMgUv2DRMI0';                              // replace the constant with the actual productId (aka. Document ID) that's being updated
+        const targetStoreId = 'Dpr0frv7ioovMXyH9oda';                                // replace the constant with the actual storeId (aka. Document ID) that's being updated
+
+        const productRef = doc(db, 'products', targetProductId);
+        const storeRef = doc(productRef, 'stores_carrying', targetStoreId);
+        await updateDoc(storeRef, {
+            price: 10                                                                // replace '3' with the new price
+        });
+    }
+
+    const resetAsyncStorage = () => {
+        AsyncStorage.getAllKeys()
+        .then(keys => AsyncStorage.multiRemove(keys))
+        .then(() => alert('success'));
+    }
     
     return (
         <View style ={styles.buttonContainer}>
